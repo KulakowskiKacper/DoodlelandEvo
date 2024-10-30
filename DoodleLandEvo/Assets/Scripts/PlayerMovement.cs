f02e2fe9df5362a2f6415a603613791ddf39cf33
Kacper <kulakowski.ka@gmail.com>
2024-10-30 16:25:21 +0100

    added AWD movement
    added movement feature
    A - move left
    D - move right
    W - Jump
    
    horizontal movement only possible when in midair, feature for stage 1 evolution

---
+++ DoodleLandEvo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpHeight;
+    private bool inAir = false;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && !inAir)
+        {
+            Jump();
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D) && inAir)
+        {
+            MoveRight();
+        }
+        if (Input.GetKey(KeyCode.A) && inAir)
+        {
+            MoveLeft();
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        inAir = false;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        inAir = true;
+    }
+
+    private void MoveRight()
+    {
+        transform.Translate(new Vector2(speed * Time.deltaTime, 0));
+    }
+    private void MoveLeft()
+    {
+        transform.Translate(new Vector2(-speed * Time.deltaTime, 0));
+    }
+    private void Jump()
+    {
+        rb.AddForce(new Vector2(0, jumpHeight), ForceMode2D.Impulse);
+    }
+
+}
